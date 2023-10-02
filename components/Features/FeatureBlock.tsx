0de344a08f0b4cf275d334e7bbe37a0adc9d8df8
mostchaotic <146731438+mostchaotic@users.noreply.github.com>
2023-10-02 07:15:22 -0600

    feat(ui): add feature blocks and reversing

---
+++ components/Features/FeatureBlock.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+interface FeatureBlockProps {
+  title: string;
+  description: string;
+  isReverse?: boolean;
+}
+
+const FeatureBlock = ({ description, isReverse = false, title}: FeatureBlockProps) => {
+  return (
+    <section className="py-16 md:py-20 lg:py-28">
+      <div className={`container `}>
+        <div className={`-mx-4 flex flex-wrap items-center  ${isReverse ? 'flex-row-reverse' : 'flex-row'} `}>
+          <div className="w-full px-4 lg:w-1/2">
+            <div
+              className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
+              data-wow-delay=".15s"
+            >
+              <Image
+                src="/images/about/about-image-2.svg"
+                alt="about image"
+                fill
+              />
+            </div>
+          </div>
+         <div className="w-full px-4 lg:w-1/2">
+            <div className="wow fadeInUp max-w-[470px]" data-wow-delay=".2s">
+              <div className="mb-9">
+                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                  {title}
+                </h3>
+                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
+                  {description}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeatureBlock;
--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -1,8 +1,24 @@
 import SectionTitle from "../Common/SectionTitle";
+import FeatureBlock from './FeatureBlock';
 import SingleFeature from "./SingleFeature";
 import featuresData from "./featuresData";
 
 const Features = () => {
+
+  const designServices = {
+    title: 'Design Services',
+    description: 'Prismatic Power boasts a team of highly skilled engineers and designers who craft tailor-made solar solutions. We take into account your specific energy needs, location, and budget to create an optimal solar system design that maximizes efficiency and savings.'
+  };
+
+  const installationExpertise = {
+    title: 'Installation Expertise',
+    description: 'Our experienced technicians ensure flawless installation, using state-of- the-art equipment and adhering to the highest industry standards. We take pride in delivering projects on time and within budget, while maintaining the utmost attention to safety.'
+  };
+
+  const ppas = {
+    title: 'Power Purchase Agreements (PPAs)',
+    description: `Prismatic Power understands that not all organizations have the upfront capital to invest in solar projects. That's why we offer flexible Power Purchase Agreements (PPAs) that allow you to adopt clean energy without the financial burden. We can finance PPAs ranging from 2 million up to an impressive 50 million dollars, providing you with a dependable and cost- effective energy source.`
+  }
   return (
     <>
       <section
@@ -10,17 +26,21 @@ const Features = () => {
         className="bg-primary/[.03] py-16 md:py-20 lg:py-28"
       >
         <div className="container">
-          <SectionTitle
+          {/* <SectionTitle
             title="Main Features"
             paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
             center
-          />
+          /> */}
 
-          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+          {/* <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
             {featuresData.map((feature) => (
               <SingleFeature key={feature.id} feature={feature} />
             ))}
-          </div>
+          </div> */}
+          <FeatureBlock description={designServices.description} title={designServices.title}  />
+          <FeatureBlock description={installationExpertise.description} isReverse={true} title={installationExpertise.title} />
+          <FeatureBlock description={ppas.description} title={ppas.title}  />
+
         </div>
       </section>
     </>
